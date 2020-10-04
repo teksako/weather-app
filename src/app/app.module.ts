14f13fa0e8d2bae749e7c2124b7c76f9ae014cf3
teksako <teksako@op.pl>
2020-10-04 10:26:22 +0200

    commit third

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,16 @@ import { SelectCityComponent } from './component/select-city/select-city.compone
 import { ShowWeatherComponent } from './component/show-weather/show-weather.component';
 import { ToolbarComponent } from './component/toolbar/toolbar.component';
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
+const materialModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +29,9 @@ import { PageNotFoundComponent } from './component/page-not-found/page-not-found
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...materialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
